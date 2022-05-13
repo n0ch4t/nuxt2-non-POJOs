a934b642a7cfa20696acccb428bea79c6377df0b
n0ch4t <devsepnine@hotmail.com>
2022-05-13 10:53:21 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="carousel-wrapper">
+      hello hello
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      axiosToken: undefined as any,
+    }
+  },
+  fetch() {
+    this.axiosToken = this.$axios.CancelToken.source();
+
+    console.log(this.axiosToken)
+  }
+})
+</script>
+
+<style>
+.sample {
+  width: 100px;
+  height: 100px;
+  margin-left: 15px;
+}
+</style>
